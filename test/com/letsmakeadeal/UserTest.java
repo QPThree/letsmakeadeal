61a6a7f6cf20100059c61abb8b1d107ac84c1675
callmejazz <jmeade4347@gmail.com>
2021-11-05 14:32:21 -0700

    Fixed flow of game, added test classes

---
+++ test/com/letsmakeadeal/UserTest.java
@@ -0,0 +1,14 @@
+package com.letsmakeadeal;
+
+import static org.junit.Assert.*;
+
+public class UserTest {
+
+    @org.junit.Test
+    public void addReward() {
+    }
+
+    @org.junit.Test
+    public void getRewards() {
+    }
+}
