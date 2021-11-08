ece83388d166c3f296f72c0d03c209676fdb084a
Quentin Young <youngqp3@gmail.com>
2021-11-08 14:47:49 -0800

    Committing host chaanges

--- src/com/letsmakeadeal/UserFactory.java
+++ src/com/letsmakeadeal/UserFactory.java
@@ -5,6 +5,9 @@ import java.util.List;
 
 public class UserFactory {
 
+    //all static factory class
+    private UserFactory(){};
+
     public static User createUser(String name, Reward reward) {
         return new User(name, reward);
     }
